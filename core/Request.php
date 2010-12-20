e3534c41f8e9796f6dd783d3f31d54c6c0029990
Roman Nowicki <peengle@gmail.com>
2010-12-20 23:42:18 +0100

    Additional method for Request
    isAjax, isCli, isHttpRequest can give us information about
    communication protocol

--- core/Request.php
+++ core/Request.php
@@ -3,10 +3,23 @@
 class Request {
     protected $get;
     protected $post;
+    protected $isAjax;
 
     public function __construct() {
         $this->get = $_GET;
         $this->post = $_POST;
+    }
+
+    public function isAjax() {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER ['HTTP_X_REQUESTED_WITH']  == 'XMLHttpRequest';
+    }
+
+    public function isCli() {
+        return isset($_SERVER['SHELL']) ? true : false;
+    }
+
+    public function isHttpRequest() {
+        return ($this->isAjax() || $this->isCli()) ? false : true;
     }
 
     public function getParam($name, $default=null) {

76f82ec0eb4d14117d76a1a7c73c845aa88ab79f
Roman Nowicki <peengle@gmail.com>
2010-12-20 23:43:51 +0100

    Added scrumie.js
    This is the place where all scrumie specific js methods should be placed

---
+++ layout/scrumie.js
@@ -0,0 +1,86 @@
+/* scrumie js */
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
+
+
+/**
+ * .disableTextSelect - Disable Text Select Plugin
+ *
+ * Version: 1.1
+ * Updated: 2007-11-28
+ *
+ * Used to stop users from selecting text
+ *
+ * Copyright (c) 2007 James Dempster (letssurf@gmail.com, http://www.jdempster.com/category/jquery/disabletextselect/)
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ **/
+(function($) {
+    if ($.browser.mozilla) {
+        $.fn.disableTextSelect = function() {
+            return this.each(function() {
+                $(this).css({
+                    'MozUserSelect' : 'none'
+                });
+            });
+        };
+        $.fn.enableTextSelect = function() {
+            return this.each(function() {
+                $(this).css({
+                    'MozUserSelect' : ''
+                });
+            });
+        };
+    } else if ($.browser.msie) {
+        $.fn.disableTextSelect = function() {
+            return this.each(function() {
+                $(this).bind('selectstart.disableTextSelect', function() {
+                    return false;
+                });
+            });
+        };
+        $.fn.enableTextSelect = function() {
+            return this.each(function() {
+                $(this).unbind('selectstart.disableTextSelect');
+            });
+        };
+    } else {
+        $.fn.disableTextSelect = function() {
+            return this.each(function() {
+                $(this).bind('mousedown.disableTextSelect', function() {
+                    return false;
+                });
+            });
+        };
+        $.fn.enableTextSelect = function() {
+            return this.each(function() {
+                $(this).unbind('mousedown.disableTextSelect');
+            });
+        };
+    }
+})(jQuery);
+
+function generateId() {
+    var date = new Date;
+    return Math.floor(Math.random() * 10000000) + '_' + date.getTime();
+}
+
+function mpr(value) {
+    console.log(value);
+}
+

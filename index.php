19e5fdd4beb677f463b3782293f536ff3ed1d65d
Roman Nowicki <peengle@gmail.com>
2010-12-20 23:41:26 +0100

    Added mpr debug helper

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 
 require_once ('core/Application.php');
+require_once('./lib/ScrumieController.php');
+session_start();
 $App = Application::getInstance();
 
-//the GET parameters in uri should look like that -> ?controller=Index&action=index 
 $App->dispatch($App->getControllerName(), $App->getActionName());
+
+//debug function
+function mpr($val, $die=false) {
+    if(!headers_sent()) 
+        header("content-type: text/plain");
+
+    if (is_array($val) || is_object($val)) {
+        print_r($val);
+
+    if(is_array($val))
+        reset($val);
+    }   
+    else
+        var_dump($val);
+
+    if($die)
+    {   
+        $trace = debug_backtrace();
+        echo "--\n";
+        echo sprintf('Who called me: %s line %s', $trace[0]['file'], $trace[0]['line']);
+        die();
+    }   
+}

6342540ed434b339414fb7c2199234df18737599
Roman Nowicki <peengle@gmail.com>
2010-12-20 23:39:51 +0100

    Modification of abstract Controller
    Added method:
    _forward, _redirect, _getParam
    
    flush data will now setup correct headears depends on communication
    protocol.
    
    Controller supports layout

--- core/Controller.php
+++ core/Controller.php
@@ -1,21 +1,56 @@
 <?php
 
 abstract class Controller {
-    protected $View;
-    protected $name;
-    protected $action;
+    protected $templateFile = 'index.phtml';
+    protected $templateDir;
+    protected $view;
+    protected $layout;
+    protected $result;
 
-    final public function __construct($action) {
-        $this->View = new View;
-        $this->View->setTemplateFile($action.'.phtml');
-        $this->View->setTemplateDir('./view/'.$this->name);
-
+    final public function __construct() {
+        $this->view = new View;
+        $this->templateDir = './view/'.strtolower(substr(get_class($this), 0, -10));
         $this->init();
     }
 
     protected function init() { }
 
     public function flush() {
-        echo (string) $this->View;
+        if(Application::getInstance()->isAjaxRequest()) {
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            header('Content-type: application/json');
+            echo json_encode($this->result);
+        }
+        else {
+            header('Content-type: text/html');
+            $this->view->setTemplateFile($this->templateFile);
+            $this->view->setTemplateDir($this->templateDir);
+            $output = (string) $this->view;
+
+            if($this->layout) {
+                $layout = new View;
+                $layout->setTemplateFile($this->layout);
+                $layout->setTemplateDir('./layout/');
+                $layout->content = $output;
+
+                $output = (string) $layout;
+                echo $output;
+            }
+        }
+
+    }
+
+    protected function _forward($controller, $action) {
+        Application::getInstance()->dispatch($controller, $action);
+    }
+
+    protected function _redirect($controller, $action) {
+        header(sprintf('Location: ?controller=%s&action=%s', $controller, $action));
+        exit;
+    }
+
+    protected function _getParam($name, $default = null) {
+        return Application::getInstance()->getRequest()->getParam($name, $default);
     }
 }

8ea2a0b70b82768ecb9a98a6e0cfba6ec193cee5
Roman Nowicki <peengle@gmail.com>
2010-12-20 23:40:59 +0100

    Added ScrumieController abstract class for all Controllers

---
+++ lib/ScrumieController.php
@@ -0,0 +1,20 @@
+<?php
+
+class ScrumieController extends Controller
+{
+    static private $_services = array();
+
+    public function getService($name) {
+        if(array_key_exists($name, self::$_services))
+            return self::$_services[$name];
+
+        require_once ('./core/Service.php');
+        require_once ('./services/'.$name.'.php');
+
+        $class = $name.'Service';
+
+        self::$_services[$name] = $service = new $class;
+
+        return $service;
+    }
+}

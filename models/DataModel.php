01f939675cc65a5bb19656387fbd661ca651e940
Roman Nowicki <peengle@gmail.com>
2010-12-21 00:59:52 +0100

    Added additional methods for abstact DataModel
    * __set_[field] can be use to overload magic __set method for specific field
    * delete implemented
    * fetchBy(column_name, value) implemented

--- models/DataModel.php
+++ models/DataModel.php
@@ -11,6 +11,11 @@ abstract class DataModel implements ActiveRecordInterface
 
     protected $data = array();
 
+    public function __construct($data = array()) {
+        foreach($data as $key => $value)
+            $this->$key = $value;
+    }
+
     public function insert() {
         $data = $this->data;
         unset($data[static::INDEX]);
@@ -40,13 +45,21 @@ abstract class DataModel implements ActiveRecordInterface
         return true;
     }
 
-    public function delete() {}
+    public function delete() {
+        $sql = sprintf("DELETE FROM %s WHERE %s = '%s'", static::TABLE, static::INDEX, $this->getId());
+        self::query($sql);
+    }
 
     static public function getById($id) {
         if(! $result = self::fetch(sprintf('SELECT * FROM %s WHERE %s = %s', static::TABLE, static::INDEX, $id)))
             throw new InvalidArgumentException(sprintf('%s with id: %s dosent exists',static::_CLASS_, $id));
 
-        return $result[0];
+        $class = static::_CLASS_;
+        return new $class($result[0]);
+    }
+
+    static public function fetchBy($column, $value) {
+        return self::fetch(sprintf("SELECT * FROM %s WHERE %s = '%s'", static::TABLE, $column, $value));
     }
 
     static public function fetchAll() {
@@ -88,7 +101,11 @@ abstract class DataModel implements ActiveRecordInterface
         if(! array_key_exists($name, $this->data))
             throw new InvalidArgumentException(sprintf('Variable %s dosen\'t exists in %s', $name, static::_CLASS_));
 
-        $this->data[$name] = $value;
+        $magic_method = '__set_'.$name;
+        if(method_exists($this, $magic_method))
+            $this->$magic_method($value);
+        else
+            $this->data[$name] = $value;
     }
 
     public function __get($name) {

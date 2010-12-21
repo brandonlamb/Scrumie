850d0f64b6989a42ccd878b32f4467fe2d10e8d4
Roman Nowicki <peengle@gmail.com>
2010-12-21 01:04:16 +0100

    dateTask added to service Task, state column included in save method
    fetchTaskForSprint fixed to avoid undefined index in view

--- services/Task.php
+++ services/Task.php
@@ -5,22 +5,34 @@ require_once ('./models/Task.php');
 class TaskService extends Service
 {
     public function fetchTaskForSprint($sprintId) {
+
+
+        $tasks['todo'] = array(); 
+        $tasks['inProgress'] = array(); 
+        $tasks['commited'] = array(); 
+        $tasks['readyForTest'] = array(); 
+        $tasks['done'] = array(); 
+
+        foreach(Task::fetchBySprintId($sprintId) as $task)
+            $tasks[$task->state][] = $task;
+
         return array(
-            'todo' => array(),
-            'inProgress' => array(),
-            'commited' => array(),
-            'readyForTest' => array(),
-            'done' => array(),
+            'todo' => $tasks['todo'],
+            'inProgress' => $tasks['inProgress'],
+            'commited' => $tasks['commited'],
+            'readyForTest' => $tasks['readyForTest'],
+            'done' => $tasks['done'],
         );
     }
 
-    public function saveTask($sprintId, $taskId, $body, $estimation, $owner) {
+    public function saveTask($sprintId, $taskId, $body, $estimation, $owner, $state) {
         $task = new Task();
         $task->id_sprint = $sprintId;
         $task->id_task = $taskId;
         $task->body = $body;
         $task->estimation = $estimation;
         $task->owner = $owner;
+        $task->state = $state;
 
         if($taskId)
             $task->update();
@@ -28,5 +40,9 @@ class TaskService extends Service
             $task->id_task = $task->insert();
 
         return $task;
+    }
+
+    public function deleteTask($taskId) {
+        Task::getById($taskId)->delete();
     }
 }

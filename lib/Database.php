22eea06acab700c127a7dfb96916d0d126131942
Roman Nowicki <peengle@gmail.com>
2010-12-23 00:31:40 +0100

    When database dosen't exists crate new one

--- lib/Database.php
+++ lib/Database.php
@@ -20,8 +20,20 @@ class Database
         if(! class_exists('PDO'))
             throw new RuntimeException('Missing PDO class');
 
-        if(! is_file(DATABASE))
-            throw new RuntimeException(sprintf('Missing %s database file', DATABASE));
+        if(! is_writable('data'))
+            throw new RuntimeException(sprintf('Directory ./data is not writable'));
+
+        if(! is_file(DATABASE)) { 
+            //when database dosen't exist this will crate new from migration file
+            $newDb = new PDO('sqlite:'.DATABASE); 
+            $sql = file_get_contents('data/scrumie.sql');
+            foreach(explode("\n", $sql) as $query) {
+                if(!$query)
+                    continue;
+
+                $result = $newDb->query($query);
+            }
+        }
 
         if(! is_readable(DATABASE))
             throw new RuntimeException(sprintf('Database %s file is not readable', DATABASE));

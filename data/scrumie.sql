f74f13f2aa64b6bcc146b1f8ce74cac908e4febe
Roman Nowicki <peengle@gmail.com>
2011-01-06 01:32:35 +0100

    Added task_history table
    In this table all task modifiaction (one per day) will be registered

--- data/scrumie.sql
+++ data/scrumie.sql
@@ -1,3 +1,4 @@
 CREATE TABLE "sprint" ("id_sprint" INTEGER PRIMARY KEY  NOT NULL ,"name" VARCHAR,"startdate" DATETIME, "estimation" INTEGER);
 CREATE TABLE "task" ("id_task" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "body" TEXT NOT NULL  check(typeof("body") = 'text') , "estimation" INTEGER NOT NULL , "owner" VARCHAR, "id_sprint" INTEGER NOT NULL , "state" VARCHAR, "done" INTEGER NOT NULL  DEFAULT 0);
+CREATE TABLE "task_history" ("id" integer PRIMARY KEY  NOT NULL  UNIQUE , "id_task" integer NOT NULL , "date" datetime NOT NULL , "done" integer NOT NULL );
 CREATE TABLE "user" ("id_user" INTEGER PRIMARY KEY  NOT NULL ,"login" VARCHAR NOT NULL ,"password" VARCHAR NOT NULL );

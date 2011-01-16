bb8cd41669828d10cfd9f6990e9765c72a65ac36
Roman Nowicki <peengle@gmail.com>
2011-01-16 08:51:46 +0100

    Update of sqlite database schema

--- data/scrumie.sql
+++ data/scrumie.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "project" ("id" INTEGER PRIMARY KEY  NOT NULL ,"name" VARCHAR NOT NULL ,"password" VARCHAR NOT NULL );
-CREATE TABLE "sprint" ("id_sprint" INTEGER PRIMARY KEY  NOT NULL ,"name" VARCHAR,"startdate" DATETIME, "id_project" integer);
-CREATE TABLE "task" ("id_task" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "body" TEXT NOT NULL  check(typeof("body") = 'text') , "estimation" INTEGER NOT NULL , "owner" VARCHAR, "id_sprint" INTEGER NOT NULL , "state" VARCHAR, "done" INTEGER NOT NULL  DEFAULT 0, "order" integer, "id_project" integer);
+CREATE TABLE "sprint" ("id" INTEGER PRIMARY KEY  NOT NULL ,"name" VARCHAR,"startdate" DATETIME, "id_project" integer);
+CREATE TABLE "task" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "body" TEXT NOT NULL  check(typeof("body") = 'text') , "estimation" INTEGER NOT NULL , "owner" VARCHAR, "id_sprint" INTEGER NOT NULL , "state" VARCHAR, "done" INTEGER NOT NULL  DEFAULT 0, "order" integer, "id_project" integer);
 CREATE TABLE "task_history" ("id" integer PRIMARY KEY  NOT NULL  UNIQUE , "id_task" integer NOT NULL , "date" datetime NOT NULL , "done" integer NOT NULL );

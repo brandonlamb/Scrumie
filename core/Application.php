3dd7919af5ff0786335279d764313843468ad14c
Roman Nowicki <peengle@gmail.com>
2011-01-16 06:09:13 +0100

    Moved debug mpr to application.php file

--- core/Application.php
+++ core/Application.php
@@ -42,3 +42,26 @@ class Application {
         return $this->FrontController->Request->isAjax();
     }
 }
+
+//debug function
+function mpr($val, $die=false) {
+    if(!headers_sent()) 
+        header("content-type: text/plain");
+
+    if (is_array($val) || is_object($val)) {
+        print_r($val);
+
+    if(is_array($val))
+        reset($val);
+    }   
+    else
+        var_dump($val);
+
+    if($die)
+    {   
+        $trace = debug_backtrace();
+        echo "--\n";
+        echo sprintf('Who called me: %s line %s', $trace[0]['file'], $trace[0]['line']);
+        die();
+    }   
+}
--- index.php
+++ index.php
@@ -5,36 +5,12 @@
  * http://scrumie.cjb.net
  */
 require_once ('./core/Application.php');
+require_once('./core/DAO.php');
+
 require_once('./controllers/ScrumieController.php');
 
-
-require_once('./core/DAO.php');
-DAO::setAdapter(new DatabaseAdapter(new PDO('sqlite:data/scrumie.sqlite')));
-
 session_start();
+
+DAO::setAdapter(new DatabaseAdapter(new PDO('sqlite:data/scrumie.sqlite')));
 $App = Application::getInstance();
-
 $App->dispatch($App->getControllerName(), $App->getActionName());
-
-//debug function
-function mpr($val, $die=false) {
-    if(!headers_sent()) 
-        header("content-type: text/plain");
-
-    if (is_array($val) || is_object($val)) {
-        print_r($val);
-
-    if(is_array($val))
-        reset($val);
-    }   
-    else
-        var_dump($val);
-
-    if($die)
-    {   
-        $trace = debug_backtrace();
-        echo "--\n";
-        echo sprintf('Who called me: %s line %s', $trace[0]['file'], $trace[0]['line']);
-        die();
-    }   
-}

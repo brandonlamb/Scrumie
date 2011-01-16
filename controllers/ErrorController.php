27bbd761f40adaef319851d1b3cccb5691eb95a6
Roman Nowicki <peengle@gmail.com>
2011-01-16 08:46:51 +0100

    Nice and meaning error message for pdo exception will be generated

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -5,6 +5,15 @@ class ErrorController extends Controller {
 
     public function index(Exception $e) {
         $this->view->error = $e;
+        $this->view->errorInfo = 'no suggestion for solution :(';
         $this->result = array('error' => $e->getMessage()); //this is for ajax call
+
+        if($e instanceof PDOException)
+            $this->pdoException($e);
+    }
+
+    public function pdoException(PDOException $e) {
+        if($e->getCode() == 'HY000')
+            $this->view->errorInfo = 'Try to update your database with liqubase';
     }
 }

4655f0238953311c8df0091785c2f6b33a8be31e
Roman Nowicki <peengle@gmail.com>
2011-09-03 14:00:31 +0200

    remove unused api functions, I resignate from Api interfaces all those method was move to model class as static methods

--- api/ProjectApi.php
+++ api/ProjectApi.php
@@ -2,24 +2,4 @@
 
 class ProjectApi extends Api
 {
-    public function registry($name, $password) {
-        
-        if(DAO::get('Project')->exists(array('name'=>$name)))
-            throw new Exception('Project already registered');
-
-        $project = new Project();
-        $project->name = $name;
-        $project->password = md5($password);
-        $project->id = DAO::insert($project);
-    }
-
-    /**
-     * When autorization fail return false otherwise returns project id
-     */
-    public function authorize($name, $password) {
-        if(! $project = DAO::get('Project')->fetchBy(array('name'=>$name, 'password' => md5($password))))
-            return false;
-
-        return $project[0]->id;
-    }
 }
--- api/SprintApi.php
+++ api/SprintApi.php
@@ -2,51 +2,6 @@
 
 class SprintApi extends Api
 {
-    public function fetchAllForProjectId($projectId) {
-        return DAO::get('Sprint')->by('id_project', $projectId);
-    }
-
-    public function getById($id) {
-        return DAO::get('Sprint')->byId($id);
-    }
-
-    public function addNewSprint($sprintName, $projectId) {
-        $sprint = new Sprint();
-        $sprint->name = $sprintName;
-        $sprint->startdate = date('Y-m-d H:i:s', time());
-        $sprint->id_project = $projectId;
-        $sprint->id = DAO::insert($sprint);
-
-        return $sprint;
-    }
-
-    public function renameSprint($sprintId, $name) {
-        $sprint = $this->getById($sprintId);
-        $sprint->name = $name;
-        DAO::update($sprint);
-    }
-
-    public function deleteSprint($sprintId) {
-        DAO::delete('Sprint', $sprintId);
-    }
-
-    public function getAllTaskIdsForSprint($sprintId) {
-        $tasks_ids = array();
-        foreach(DAO::get('Task')->by('id_sprint', $sprintId) as $task)
-            $tasks_ids[] = $task->id;
-        return $tasks_ids;
-    }
-
-    public function getSprintUpdateDates($sprintId) {
-        $tasks_ids = $this->getAllTaskIdsForSprint($sprintId);
-        $updateDates = $this->getApi('Task')->getTasksUpdateDates($tasks_ids);
-        return $updateDates;
-    }
-
-    public function getSprintEstimation($sprintId) {
-        return DAO::query("select sum(estimation) as sum from task where id_sprint = $sprintId")->pop()->sum;
-    }
-
     public function getEstimationForEachSprintDate($sprintId) {
         $tasks_ids = $this->getAllTaskIdsForSprint($sprintId);
         $updateDates = $this->getSprintUpdateDates($sprintId);
